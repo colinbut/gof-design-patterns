7e75e217f8a8c835d0bd1b5d8e2af900169a1386
Colin But <colinbut@users.noreply.github.com>
2019-06-22 19:16:56 +0100

    copyright

--- abstractfactory/src/main/java/com/personal/codelib/softwarepatterns/designpattern/abstractfactory/AbstractFactory.java
+++ abstractfactory/src/main/java/com/personal/codelib/softwarepatterns/designpattern/abstractfactory/AbstractFactory.java
@@ -1,5 +1,7 @@
-/**
- * 
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2012 Colin But. All rights reserved.
+ * |-------------------------------------------------
  */
 package main.java.com.personal.codelib.softwarepatterns.designpattern.abstractfactory;
 
