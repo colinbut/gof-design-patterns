0b6fa9a7662cc347eda0f72a66d03069f0d18054
Colin But <colinbut@users.noreply.github.com>
2019-06-22 21:32:21 +0100

    Bridge pattern

---
+++ bridge/src/main/java/com/personal/codelib/softwarepatterns/designpattern/bridge/Implementor.java
@@ -0,0 +1,19 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2012 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package main.java.com.personal.codelib.softwarepatterns.designpattern.bridge;
+
+/**
+ * @author Administrator
+ *
+ */
+public abstract class Implementor {
+	
+	/**
+	 * 
+	 */
+	public abstract void operation();
+
+}
