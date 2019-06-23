8fcc7c3574a46806241c93e0c6bafb10b67c86eb
Colin But <colinbut@users.noreply.github.com>
2019-06-23 22:12:33 +0100

    mavenised composite design pattern

---
+++ composite/src/main/java/com/personal/codelib/softwarepatterns/designpattern/composite/Component.java
@@ -0,0 +1,21 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2012 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package main.java.com.personal.codelib.softwarepatterns.designpattern.composite;
+
+/**
+ * Representation of a component 
+ * 
+ * @author Administrator
+ *
+ */
+public interface Component {
+	
+	/**
+	 * Performs an operation
+	 */
+	public void operation();
+
+}
