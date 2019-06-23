d4cee04565e008c8480717745a74ed52e401750a
Colin But <colinbut@users.noreply.github.com>
2019-06-23 22:07:49 +0100

    mavenised command pattern

---
+++ command/src/main/java/com/personal/codelib/softwarepatterns/designpattern/command/Command.java
@@ -0,0 +1,19 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2012 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package main.java.com.personal.codelib.softwarepatterns.designpattern.command;
+
+/**
+ * @author Administrator
+ *
+ */
+public interface Command {
+	
+	/**
+	 * Fires the command
+	 */
+	public void execute();
+
+}
