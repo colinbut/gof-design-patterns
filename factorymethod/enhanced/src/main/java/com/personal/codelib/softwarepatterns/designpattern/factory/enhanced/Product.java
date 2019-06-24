7635363bce79576f6bd11eebf351412df64ace6a
Colin But <colinbut@users.noreply.github.com>
2019-06-24 23:36:10 +0100

    mavenised factory method - enhanced version - design pattern

--- factorymethod/enhanced/src/main/java/com/personal/codelib/softwarepatterns/designpattern/factory/enhanced/Product.java
+++ factorymethod/enhanced/src/main/java/com/personal/codelib/softwarepatterns/designpattern/factory/enhanced/Product.java
@@ -1,7 +1,9 @@
-/**
- * 
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2012 Colin But. All rights reserved.
+ * |-------------------------------------------------
  */
-package com.personal.codelib.softwarepatterns.designpattern.factorymethod.enhanced;
+package main.java.com.personal.codelib.softwarepatterns.designpattern.factory.enhanced;
 
 /**
  * @author Administrator
