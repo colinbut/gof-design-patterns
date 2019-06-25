5b92d5df3157466cc830189aee2b3d27c10e8503
Colin But <colinbut@users.noreply.github.com>
2019-06-25 08:46:58 +0100

    mavenised flyweight design pattern

---
+++ flyweight/src/main/java/com/personal/codelib/softwarepatterns/designpattern/flyweight/Flyweight.java
@@ -0,0 +1,22 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2012 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package main.java.com.personal.codelib.softwarepatterns.designpattern.flyweight;
+
+/**
+ * 
+ * @author Administrator
+ *
+ */
+public interface Flyweight {
+	
+	/**
+	 * Do something arbitrary
+	 * 
+	 * @param context the context to act upon
+	 */
+	public void doSomething(Context context);
+
+}
