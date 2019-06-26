65a4a7b4cb05cb4ff9c0ab260dfd115dea965728
Colin But <colinbut@users.noreply.github.com>
2019-06-26 19:59:47 +0100

    mavenised mediator design pattern

---
+++ mediator/src/main/java/com/personal/codelib/softwarepatterns/designpattern/mediator/Command.java
@@ -0,0 +1,19 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2012 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package main.java.com.personal.codelib.softwarepatterns.designpattern.mediator;
+
+/**
+ * 
+ * @author Administrator
+ *
+ */
+public interface Command {
+
+	/**
+	 * Execute an action
+	 */
+	public void execute();
+}
