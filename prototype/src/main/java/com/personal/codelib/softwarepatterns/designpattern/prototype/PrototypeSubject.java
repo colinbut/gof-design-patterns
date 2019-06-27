080a9b14968c001e4256a8c4bd444116e689f6f3
Colin But <colinbut@users.noreply.github.com>
2019-06-27 21:45:51 +0100

    mavenised prototype design pattern

--- prototype/src/main/java/com/personal/codelib/softwarepatterns/designpattern/prototype/PrototypeSubject.java
+++ prototype/src/main/java/com/personal/codelib/softwarepatterns/designpattern/prototype/PrototypeSubject.java
@@ -1,7 +1,9 @@
-/**
- * 
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2012 Colin But. All rights reserved.
+ * |-------------------------------------------------
  */
-package com.personal.codelib.softwarepatterns.designpattern.prototype;
+package main.java.com.personal.codelib.softwarepatterns.designpattern.prototype;
 
 /**
  * The prototype object that gets created many times.
